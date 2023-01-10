a5470bd821cf3274e5f178369f5a92c09c91cb27
504 <504@DESKTOP-VVQ86L0>
2023-01-10 17:15:08 +0900

    23/01/10 과제

--- front/public/과제/js/과제2.js
+++ front/public/과제/js/과제2.js
@@ -1,5 +1,5 @@
 function Signup(){
-	let check = document.querySelector('#Sbox').value
+	let check = document.querySelector('#Sbox2').value
 
 	let check2 = studentArray.indexOf(check);
 	
@@ -15,15 +15,16 @@ function Signup(){
 		if(check2>-1)
 		{// 배열 존재시
 		alert("존재하는 학번 입니다.")
-		document.querySelector('.resultBox').innerHTML="존재하는 학번 입니다.";
+		document.querySelector('.resultBox2').innerHTML="존재하는 학번 입니다.";
 		}
 		else
 		{ //배열에 존재하지 않을시	
-		studentArray.push(document.querySelector('#Sbox').value)
-		alert("학번등록되었습니다")
-		document.querySelector('#Sbox').value = null;
-		document.querySelector('.resultBox').innerHTML="등록되었습니다.";
+		studentArray.push(check)
 		console.log(studentArray)
+		alert("학번등록되었습니다")
+		
+		document.querySelector('#Sbox2').value = null;
+		document.querySelector('.resultBox2').innerHTML="등록되었습니다.";
 		}
 	}
 }
