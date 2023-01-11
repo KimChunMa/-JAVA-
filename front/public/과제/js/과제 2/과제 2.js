559c0c0baf8ff629d3c15f3d150a7124dde44c8d
504 <504@DESKTOP-VVQ86L0>
2023-01-11 15:55:37 +0900

    23/01/11 과제 업데이트

--- front/public/과제/js/과제 2/과제 2.js
+++ front/public/과제/js/과제 2/과제 2.js
@@ -1,9 +1,19 @@
-let 단 = Number(prompt("단을 입력하세용"))
-let 곱 = Number(prompt("곱을 입력하세용"))
-
-
+let 단 = Number(prompt("단을 입력하세용")) //메세지로 입력하기
+let 곱 = Number(prompt("곱을 입력하세용")) //메세지로 입력하기
 
 function On(){
+
+//텍스트로 입력할때
+let dan =  Number(document.querySelector('#dan').value)
+let multiply = Number(document.querySelector('#multiply').value) 
+
+if(dan && multiply)
+{
+ 단 = dan;
+ 곱 = multiply;
+}
+
+
 let tbl = '<tr> <th> 단 </th> <th> 곱 </th> <th> 결과 </th> </tr>';
 
 	for(let i = 1 ; i <= 곱 ;i++ )
