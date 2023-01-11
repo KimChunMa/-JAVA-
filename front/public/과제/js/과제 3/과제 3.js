ca8155503694ce7a7e1484eae843cce606018440
504 <504@DESKTOP-VVQ86L0>
2023-01-11 17:16:39 +0900

    23/01/11 방명록 과제

--- front/public/과제/js/과제 3/과제 3.js
+++ front/public/과제/js/과제 3/과제 3.js
@@ -1,21 +1,31 @@
-let contentArrays = [];
+/*방명록 배열*/
+let contentArrays = []; 
 
 
+/*버튼 기능 */
 function addContent(){
+	
+	/*방명록 배열 넣기 */
 	contentArrays.push(document.querySelector('#box').value);
 
+/*초기 방명록 테이블 헤드*/
 let table = ' <table> <tr> <th> 번호 </th> <th> 방문록 </th> </tr>'	
+
+
+	/*방명록 배열 길이까지 행 늘리기 */
 	for(let i = 0 ; i < contentArrays.length ; i ++)
 	{
 		table += '<tr>'
 			table += '<td>'+ (i+1) +'</td>'
 			table += '<td>'+ contentArrays[i] +'</td>'
-		
 		table += '</tr>'
 		
 	}
+	
+	/*방명록 테이블 끝*/
 	table += '</table>'
 
+	/*덮어쓰기*/
 	document.querySelector('#write').innerHTML = table;
 
 }
