9a08450d09d13d29662957bf630f89917794a866
504 <504@DESKTOP-VVQ86L0>
2023-01-16 09:14:53 +0900

    23/01/14

--- front/public/과제/js/과제5/도서관리시스템.js
+++ front/public/과제/js/과제5/도서관리시스템.js
@@ -1,8 +1,8 @@
-/*이경석 하단ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
+/*이경석 하단ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
 //현재 책
  let RentList = ['혼자 공부하는 자바','이것이 자바다','열혈 C언어'];
  
- //테이블시작
+ //테이블시작 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
  let bookList = `<table> 
  						<tr> 
  							<th>번호</th> <th>도서</th>
@@ -11,7 +11,7 @@
  
  //함수는 끝따옴표
  for(let i = 0 ; i<RentList.length ;i++){
-	 bookList += `<tr> 
+	 bookList += `<tr>  
 	 				<td>${i+1}</td>
 	 				<td>${RentList[i]}</td>
 	 				<td class="book${i}">대여가능</td>
@@ -20,6 +20,7 @@
 	 				<td><input type='button'
 	 				onclick="buttonRent( ${i} )"
 	 				value="[대여버튼]" id="rental${i}"
+	 				
 	 				/></td>
 	 				
 	 				<td><input type='button'
@@ -31,29 +32,30 @@
  }
 bookList+= `</table>`;
 
+//고객페이지 밑에 붙여넣기
 document.querySelector('#book_Checkout').innerHTML = bookList;
-//테이블 끝
+//테이블 끝 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 
 //대여버튼
 function buttonRent( i ){
-	const target = document.querySelector('#rental'+i);
+	const target = document.querySelector(`#rental${i}`); //대여버튼을 누르면, 대여버튼 비활성화
 		target.disabled = true;
 		
-		const target2 = document.querySelector('#return'+i);
+		const target2 = document.querySelector('#return'+i);  //대여버튼을 누르면, 반납버튼 활성화
 		target2.disabled =false;
 	
 	alert((i+1)+"번째 책, "+RentList[i] +"  가(이)  대여가 되었습니다")
-	document.querySelector('.book'+i).innerHTML = "대여중";
+	document.querySelector(`.book${i}`).innerHTML = "대여중";
 	
 }
 
 //반납버튼
 function buttonReturn( i ){
-	const target = document.querySelector('#rental'+i);
+	const target = document.querySelector(`#rental${i}`); //반납버튼을 누르면, 대여버튼 활성화
 		target.disabled = false;
 		
-		const target2 = document.querySelector('#return'+i);
+		const target2 = document.querySelector('#return'+i); //반납버튼을 누르면, 반납버튼 비활성화
 		target2.disabled =true;
 	
 	alert((i+1) +"번쨰 책, "+RentList[i] +"  가(이)  반납이 되었습니다")
