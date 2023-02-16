389e899d12e04d3740156d76d5d9fb442d12c152
504 <504@DESKTOP-KHUH3DD>
2023-02-16 14:22:39 +0900

    2023 02 16

--- backend/src/과제/과제8/model/지역/광주.java
+++ backend/src/과제/과제8/model/지역/광주.java
@@ -1,15 +1,15 @@
 package 과제.과제8.model.지역;
 
 public class 광주 {
-	//싱글톤nce() {
-		return distanc
+	//싱글톤
 	private 광주() {};
 	private static 광주 Gwangju = new 광주();
 	public static 광주 getInstance() {return Gwangju;}
 	
-	private int distance = 18;
+	private int distance = 12;
 	
-	public int getDistae;
+	public int getDistance() {
+		return distance;
 	}
 
 	public void setDistance(int distance) {
