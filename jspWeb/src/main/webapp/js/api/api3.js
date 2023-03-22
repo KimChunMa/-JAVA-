2dc079112b8d55c091876a6f1bedd9ad821a204c
504 <504@DESKTOP-KHUH3DD>
2023-03-22 10:31:07 +0900

    2023-03-22 클러스터러 완

--- jspWeb/src/main/webapp/js/api/api3.js
+++ jspWeb/src/main/webapp/js/api/api3.js
@@ -96,7 +96,7 @@ $.ajax({
 		})//forEach e
 	
 		
-		// onClusterer(markers); // 클러스터러 함수
+		
 	}//success e
 })// $.ajax e
 
@@ -110,25 +110,6 @@ $.ajax({
     });
 
 
-//현재 약국좌표가 저장된 xy(=markers)를 map으로 하려했으나 map이 실행이안됨 
-function onClusterer(markers){
-	console.log(markers)	
-        // 데이터에서 좌표 값을 가지고 마커를 표시합니다
-        // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
-        var markerss = markers.map((o)=> {
-			console.log('a'); //map 실행이 안됨
-			console.log(o.La)
-			let marker =  new kakao.maps.Marker({
-            position : new kakao.maps.LatLng(o.La, o.Ma),
-            image: markerImage 
-            });
-			
-           return marker;
-        });
-		console.log(markerss);
-        // 클러스터러에 마커들을 추가합니다
-        
- };
 
 
 
